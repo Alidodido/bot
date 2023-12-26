e25260c17da22b5d17d1f10f73447553bfa0bcea
Codidodido <139787880+Codidodido@users.noreply.github.com>
2023-12-26 15:55:23 +0330

    Add set_command php file

---
+++ functions/set_command.php
@@ -0,0 +1,17 @@
+<?php
+
+$comandos = [
+["command" => "start", "description" => "start"],
+["command" => "whois", "description" => "who is me"],
+["command" => "calc", "description" => "calculate"],
+];
+
+defineMenuOptions($comandos);
+
+function defineMenuOptions($comandos) {
+
+$comandosEnc = "setMyCommands?commands=" . json_encode($comandos);
+$retorno = file_get_contents("https://api.telegram.org/bot6966772493:AAH2kFVqY_e9pUGeil6qZvIOhxsJGmEEX90/{$comandosEnc}");
+
+}
+?>
